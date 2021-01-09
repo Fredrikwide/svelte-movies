11453ec73a8fb153429fb8f9ab8e9a09333d0c54
fredrikwide <fredrik.widegren@gmail.com>
2021-01-09 12:35:38 +0100

    inital commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  //components
+  import Home from './pages/Home.svelte';
+</script>
+
+<Home />
+
+<style>
+  :global(body) {
+    font-family: 'Abel', sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+</style>
